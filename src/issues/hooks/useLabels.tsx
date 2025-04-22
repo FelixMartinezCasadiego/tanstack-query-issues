0692f9afcf30adb08718b6eba93bb481ecfaff14
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-04-22 21:44:33 +0200

    chore: update IssueItem.tsx with fadeIn

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -6,6 +6,28 @@ export function useLabels() {
     queryKey: ["labels"],
     queryFn: getLabels,
     staleTime: 1000 * 60 * 60, // 1 hour stale time
+
+    // placeholderData: [
+    //   {
+    //     id: 69105358,
+    //     node_id: "MDU6TGFiZWw2OTEwNTM1OA==",
+    //     url: "https://api.github.com/repos/facebook/react/labels/Browser:%20Safari",
+    //     name: "Browser: Safari",
+    //     color: "c7def8",
+    //     default: false,
+    //   } satisfies GithubLabel,
+    // ],
+
+    // initialData: [
+    //   {
+    //     id: 69105358,
+    //     node_id: "MDU6TGFiZWw2OTEwNTM1OA==",
+    //     url: "https://api.github.com/repos/facebook/react/labels/Browser:%20Safari",
+    //     name: "Browser: Safari",
+    //     color: "c7def8",
+    //     default: false,
+    //   } satisfies GithubLabel,
+    // ],
   });
 
   return { labelsQuery };
