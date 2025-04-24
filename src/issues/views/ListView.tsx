b5a0f3211f4d6ba1c5e53e43dfcc43113a88d7f0
Felix Martinez Casadiego <felix.martinez@simonelectric.com>
2025-04-24 21:48:33 +0200

    feat: update IssueItem with data from the backend

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -6,10 +6,16 @@ import useIssues from "../hooks/useIssues";
 export const ListView = () => {
   const { issuesQuery } = useIssues();
 
+  const issues = issuesQuery.data ?? [];
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 mt-5">
       <div className="col-span-1 sm:col-span-2">
-        {issuesQuery.isLoading ? <LoadingSpinners /> : <IssueList />}
+        {issuesQuery.isLoading ? (
+          <LoadingSpinners />
+        ) : (
+          <IssueList issues={issues} />
+        )}
       </div>
 
       <div className="col-span-1 px-2">
